fc18c5833fb2f0def7e89fd6c03b7e91381e48b4
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-04-26 12:25:39 -0400

    chore: adds route for testing

---
+++ src/app/post-body/next-fileupload/processMultipart.ts
@@ -0,0 +1,97 @@
+import Busboy from 'busboy'
+import httpStatus from 'http-status'
+import { APIError } from 'payload/errors'
+
+import type { NextFileUploadOptions, NextFileUploadResponse } from './index'
+
+import { fileFactory } from './fileFactory'
+import { memHandler, tempFileHandler } from './handlers'
+import { processNested } from './processNested'
+import { createUploadTimer } from './uploadTimer'
+import { buildFields, debugLog, isFunc, parseFileName } from './utilities'
+import { PayloadRequest } from 'payload/types'
+
+const waitFlushProperty = Symbol('wait flush property symbol')
+
+type ProcessMultipart = (args: {
+  options: NextFileUploadOptions
+  request: PayloadRequest
+}) => Promise<NextFileUploadResponse>
+export const processMultipart: ProcessMultipart = async ({ options, request }) => {
+  let parsingRequest = true
+
+  let fileCount = 0
+  let filesCompleted = 0
+  let allFilesHaveResolved: (value?: unknown) => void
+  let failedResolvingFiles: (err: Error) => void
+
+  const allFilesComplete = new Promise((res, rej) => {
+    allFilesHaveResolved = res
+    failedResolvingFiles = rej
+  })
+
+  const result: NextFileUploadResponse = {
+    fields: undefined,
+    files: undefined,
+  }
+
+  const headersObject = {}
+  request.headers.forEach((value, name) => {
+    headersObject[name] = value
+  })
+
+  const busboy = Busboy({ ...options, headers: headersObject, debug: true })
+
+  // Build multipart req.body fields
+  busboy.on('field', (field, val) => {
+    result.fields = buildFields(result.fields, field, val)
+  })
+
+  busboy.on('finish', async () => {
+    debugLog(options, `Busboy finished parsing request.`)
+    if (options.parseNested) {
+      result.fields = processNested(result.fields)
+      result.files = processNested(result.files)
+    }
+
+    if (request[waitFlushProperty]) {
+      try {
+        await Promise.all(request[waitFlushProperty]).then(() => {
+          delete request[waitFlushProperty]
+        })
+      } catch (err) {
+        debugLog(options, `Error waiting for file write promises: ${err}`)
+      }
+    }
+
+    return result
+  })
+
+  busboy.on('error', (err) => {
+    debugLog(options, `Busboy error`)
+    parsingRequest = false
+    throw new APIError('Busboy error parsing multipart request', httpStatus.BAD_REQUEST)
+  })
+
+  if (!request.searchParams.has('skip-reader')) {
+    console.log(request.body)
+    const reader = request.body.getReader()
+    // Start parsing request
+    while (parsingRequest) {
+      const { done, value } = await reader.read()
+
+      if (done) {
+        parsingRequest = false
+      }
+
+      if (value) {
+        console.log({ value })
+        busboy.write(value)
+      }
+    }
+
+    // if (fileCount !== 0) await allFilesComplete
+  }
+  console.log({ result })
+  return result
+}
